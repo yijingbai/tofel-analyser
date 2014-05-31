be4927457885787202ae23048c9297900a8a6377
yijingbai <everatnight@gmail.com>
2014-05-31 20:38:36 +0800

    define the model

--- tofel_analyser/models.py
+++ tofel_analyser/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class Grade(models.model):
+    testpaper = model.CharField(max_length=1000)
+    qnumber = model.IntegerField()
+    qtype = model.CharField(max_length=1000)
     grade = model.FloatField()
+    createtime = models.DateTimeField(auto_now=True, auto_now_add=True)
+
+
+
