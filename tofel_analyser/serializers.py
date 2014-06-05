13ae7564e422519abf98877616109dd686ffe31d
yijingbai <everatnight@gmail.com>
2014-06-05 22:53:55 +0800

    update apiview

--- tofel_analyser/serializers.py
+++ tofel_analyser/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from models import *
 
 
-class Serializer(serializers.HyperlinkedModelSerializer):
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
         fields = ('id', 'qfrom', 'qnumber', 'qtype', 'grade', 'createtime')
