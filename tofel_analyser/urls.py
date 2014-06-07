70bb96cc08a49f65342b3f955a7701395deef688
yijingbai <everatnight@gmail.com>
2014-06-07 14:59:43 +0800

    change url

--- tofel_analyser/urls.py
+++ tofel_analyser/urls.py
@@ -9,10 +9,4 @@ router.register(r'questions', QuestionViewSet)
 urlpatterns = patterns('views',
     url(r'^api/method$', Method.as_view()),
     url(r'^api/', include(router.urls)),
-    url(r'^api/meta/(?P<tableid>.*)$', Metas.as_view()),
-        #test
-    url(r'^do$',Main),
-    url(r'^submit$',Submit),
-    url(r'^instance$',CheckStatus),
-    # url(r'^test$',test),
 )
