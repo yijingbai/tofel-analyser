3ac71eddf7b444a909467a87e8625a64c13a257b
yijingbai <everatnight@gmail.com>
2014-06-14 20:21:39 +0800

    add article serialised

--- tofel_analyser/serializers.py
+++ tofel_analyser/serializers.py
@@ -8,3 +8,10 @@ class QuestionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Question
         fields = ('id', 'qfrom', 'qnumber', 'qtype', 'grade', 'createtime')
+
+
+class Article(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('id', 'atitle', 'afrom', 'anumber', 'atype','argument','duration','user','createtime')
+
