f20f709b168a37d5b2ce0dbaad77e0511d120bd4
yijingbai <everatnight@gmail.com>
2014-06-17 23:07:05 +0800

    add

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/1.6/ref/settings/w
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
