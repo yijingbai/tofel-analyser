e4291443991944716ac1262e919c1e621751562c
yijingbai <everatnight@gmail.com>
2014-06-18 21:08:37 +0800

    update apiview

--- tofel_analyser/apiview.py
+++ tofel_analyser/apiview.py
@@ -8,11 +8,13 @@ import sys
 import os
 from rest_framework import viewsets
 
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 #导入celery worker tasks
 sys.path.append(os.path.join(BASE_DIR,"../"))
 # from worker import tasks
+
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
